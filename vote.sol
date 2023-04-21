e375740ecc37e8601d8e55eb0fc5183fc64978f8
Aman uttam <81846308+Amanuttam1192@users.noreply.github.com>
2023-04-21 19:05:25 +0530

    Update vote.sol

--- vote.sol
+++ vote.sol
@@ -103,6 +103,35 @@ contract Ballot {
         }
         return isFound;
     }
+        constructor(){
+        addCandidate("Aayush");
+        addCandidate("Aishwary");
+        addCandidate("Aman(og)");
+    }
+
+    function addCandidate(string memory _name) private {
+        candidatesCount++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+    }
+
+    function vote(uint256 _candidateId) public {
+        // require that they haven't voted before
+        require(!voters[msg.sender]);
+
+        // require a valid candidate
+        require(_candidateId > 0 && _candidateId <= candidatesCount);
+
+        // record that voter has voted
+        voters[msg.sender] = true;
+
+        // update candidate vote Count
+        candidates[_candidateId].voteCount++;
+
+        // trigger voted event
+        //emit votedEvent(_candidateId);
+    }
+}
+
     function endVote() 
         public
         inState(State.Voting)
